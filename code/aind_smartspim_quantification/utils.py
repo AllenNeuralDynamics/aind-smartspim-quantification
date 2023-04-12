e3cf6b07b41d121a4d2d1779f52113b50b17bf28
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2023-04-12 12:20:02 -0700

    refactoring project structure to work with less parameters

--- code/aind_smartspim_quantification/utils.py
+++ code/aind_smartspim_quantification/utils.py
@@ -4,27 +4,29 @@
 Created on Fri Jan 13 13:30:28 2023
 
 @author: nicholas.lusk
+@modified by: camilo.laiton
 """
 
-import os
-import ray
 import copy
 import json
+import os
 import pickle
-import vtkplotter
 
 import numpy as np
 import pandas as pd
+import ray
+import vtkplotter
 
 # initialize for multiprocessing
 if not ray.is_initialized():
-    ray.init(num_cpus = os.cpu_count(), ignore_reinit_error = True)
+    ray.init(num_cpus=os.cpu_count(), ignore_reinit_error=True)
+
 
 @ray.remote
 def parallel_func(shared_coords, shared_path, struct, struct_tup):
     """
     Function to run multiprocess counting across regions
-    
+
     Parameters
     ------------------------
     shared_coords: array
@@ -35,114 +37,123 @@ def parallel_func(shared_coords, shared_path, struct, struct_tup):
         the CCF acromym for the given structure
     struct_tup: tuple
         contains the CCF structure ID and whether crosses the midline
-        
-        
+
+
     Returns
     ------------------------
     data_out: tuple
         has identification and count information for structure being processed
-    
+
     """
-    
-    mesh_dir = os.path.join(shared_path, 'json_verts_float/')
-    with open('{}{}.json'.format(mesh_dir, struct_tup[0])) as f:
+
+    mesh_dir = os.path.join(shared_path, "json_verts_float/")
+    with open("{}{}.json".format(mesh_dir, struct_tup[0])) as f:
         structure_data = json.loads(f.read())
-        vertices,faces = (np.array(structure_data[str(struct_tup[0])]['vertices']),
-                          np.array(structure_data[str(struct_tup[0])]['faces']))
-        
+        vertices, faces = (
+            np.array(structure_data[str(struct_tup[0])]["vertices"]),
+            np.array(structure_data[str(struct_tup[0])]["faces"]),
+        )
+
         region = vtkplotter.Mesh([vertices, faces])
-        count = len(region.insidePoints(shared_coords).points())
-        
-        if struct_tup[1] == 'hemi':    
+        count = len(region.insidePoints(shared_coords))  # .points())
+
+        if struct_tup[1] == "hemi":
             L_count = copy.copy(count)
-            
+
             vertices_right = copy.copy(vertices)
-            vertices_right[:,0] = vertices_right[:,0]+(5700-vertices_right[:,0])*2
-            
+            vertices_right[:, 0] = (
+                vertices_right[:, 0] + (5700 - vertices_right[:, 0]) * 2
+            )
+
             R_region = vtkplotter.Mesh([vertices_right, faces])
-            R_count = len(R_region.insidePoints(shared_coords).points())
-            
+            R_count = len(R_region.insidePoints(shared_coords))  # .points())
+
             count = L_count + R_count
-            
-        else:  
+
+        else:
             L_count, R_count = np.NaN, np.NaN
-            
-        
+
         data_out = (struct_tup[0], struct, struct_tup[1], L_count, R_count, count)
-        
+
         return data_out
 
-class CellCounts(): 
+
+class CellCounts:
     """
     Class of getting regional cell counts
-    
+
     Parameters
     ------------------------
     resolution: int
         resolution of registration in microns
 
     """
-    
-    def __init__(self, resolution):
-        self.resolution = 25
-        self.annot_map = self.get_annotation_map()
-        self.CCF_dir = os.path.join(os.getcwd(), 'CCF_meshes')
-        self.region_files = ['non_crossing_structures', 'mid_crossing_structures']
 
+    def __init__(self, ccf_dir: str, annotation_map_path: str, resolution: int = 25):
+        self.resolution = resolution
+        self.annot_map = self.get_annotation_map(
+            os.path.join(ccf_dir, "annotation_map.json")
+        )
+        self.CCF_dir = os.path.join(ccf_dir, "CCF_meshes")
+        self.region_files = ["non_crossing_structures", "mid_crossing_structures"]
 
-    def get_annotation_map(self):
+    def get_annotation_map(self, annotation_map_path: str):
         """
         Load .json will anotation dictionary {structure_id: structure_acronym}
         """
-    
-        with open('annotation_map.json') as json_file:
+
+        with open(annotation_map_path) as json_file:
             mapping = json.load(json_file)
-           
+
         return mapping
 
     def get_CCF_mesh_points(self, structure_id):
         """
         Gets mesh of specific brain region based on structure ID
         """
-        
-        mesh_dir = os.path.join(self.CCF_dir, 'json_verts_float/')
-        with open('{}{}.json'.format(mesh_dir, structure_id)) as f:
+
+        mesh_dir = os.path.join(self.CCF_dir, "json_verts_float/")
+        with open("{}{}.json".format(mesh_dir, structure_id)) as f:
             structure_data = json.loads(f.read())
-            vertices,faces = (np.array(structure_data[str(structure_id)]['vertices']),
-                              np.array(structure_data[str(structure_id)]['faces']))
-            return vertices,faces
+            vertices, faces = (
+                np.array(structure_data[str(structure_id)]["vertices"]),
+                np.array(structure_data[str(structure_id)]["faces"]),
+            )
+            return vertices, faces
 
     def reflect_about_midline(self, vertices):
         """
         Brain regions that do not span the midline need to be flipped to get counts on rightside
         """
-        
+
         vertices_right = copy.copy(vertices)
-        vertices_right[:,0] = vertices_right[:,0]+(5700-vertices_right[:,0])*2
+        vertices_right[:, 0] = vertices_right[:, 0] + (5700 - vertices_right[:, 0]) * 2
         return vertices_right
 
     def get_region_lists(self):
         """
         Import list of acronyms of brain regions
         """
-        
-        with open(os.path.join(self.CCF_dir, self.region_files[0]),'rb') as f:
+
+        # Reading non-crossing structures to get acronyms
+        with open(os.path.join(self.CCF_dir, self.region_files[0]), "rb") as f:
             hemi_struct = pickle.load(f)
-            hemi_struct.remove(1051) # don't know why this is being done
-            hemi_labeled = [(s, 'hemi') for s in hemi_struct]
-            
-        with open(os.path.join(self.CCF_dir, self.region_files[1]), 'rb') as f:
+            hemi_struct.remove(1051)  # don't know why this is being done
+            hemi_labeled = [(s, "hemi") for s in hemi_struct]
+
+        # Reading mid-crossing structures to get acronyms
+        with open(os.path.join(self.CCF_dir, self.region_files[1]), "rb") as f:
             u = pickle._Unpickler(f)
-            u.encoding = 'latin1'
+            u.encoding = "latin1"
             mid_struct = u.load()
-            mid_labeled = [(s, 'mid') for s in mid_struct]
-            
+            mid_labeled = [(s, "mid") for s in mid_struct]
+
         self.structs = hemi_labeled + mid_labeled
-        
+
     def create_counts(self, cells):
         """
         Import list of acronyms of brain regions
-        
+
         Parameters
         ------------------------
         cells: list
@@ -152,29 +163,46 @@ class CellCounts():
         struct_count: df
             dataframe with cell counts for each brain region
         """
-        
+
         # convert cells to np.array() and convert to microns for counting
         cells = np.array(cells) * self.resolution
-        
+
         # get list of all regions and region IDs from .json file
-        self.get_region_lists()    
-                  
+        self.get_region_lists()
+
         shared_coords = ray.put(cells)
         shared_path = ray.put(self.CCF_dir)
-        
-        results = [parallel_func.remote(shared_coords, shared_path, self.annot_map[str(s[0])], s)for s in self.structs]
+
+        results = [
+            parallel_func.remote(
+                shared_coords, shared_path, self.annot_map[str(s[0])], s
+            )
+            for s in self.structs
+        ]
         data_out = ray.get(results)
-        
-        cols = ['Id','Structure','Struct_Info', 'Left', 'Right', 'Total']
-        df_out = pd.DataFrame(data_out, columns = cols)
+
+        cols = ["Id", "Structure", "Struct_Info", "Left", "Right", "Total"]
+        df_out = pd.DataFrame(data_out, columns=cols)
         return df_out
-    
 
-    
 
-    
-        
-        
-        
-        
+def read_json_as_dict(filepath: str) -> dict:
+    """
+    Reads a json as dictionary.
+    Parameters
+    ------------------------
+    filepath: PathLike
+        Path where the json is located.
+    Returns
+    ------------------------
+    dict:
+        Dictionary with the data the json has.
+    """
 
+    dictionary = {}
+
+    if os.path.exists(filepath):
+        with open(filepath) as json_file:
+            dictionary = json.load(json_file)
+
+    return dictionary
---
+++ code/main.py
@@ -0,0 +1,93 @@
+"""
+Main file to execute the smartspim segmentation
+in code ocean
+"""
+
+import subprocess
+import sys
+
+from aind_smartspim_quantification import quantification
+
+
+def save_string_to_txt(txt: str, filepath: str, mode="w") -> None:
+    """
+    Saves a text in a file in the given mode.
+
+    Parameters
+    ------------------------
+    txt: str
+        String to be saved.
+
+    filepath: PathLike
+        Path where the file is located or will be saved.
+
+    mode: str
+        File open mode.
+
+    """
+
+    with open(filepath, mode) as file:
+        file.write(txt + "\n")
+
+
+def execute_command_helper(command: str, print_command: bool = False) -> None:
+    """
+    Execute a shell command.
+
+    Parameters
+    ------------------------
+    command: str
+        Command that we want to execute.
+    print_command: bool
+        Bool that dictates if we print the command in the console.
+
+    Raises
+    ------------------------
+    CalledProcessError:
+        if the command could not be executed (Returned non-zero status).
+
+    """
+
+    if print_command:
+        print(command)
+
+    popen = subprocess.Popen(
+        command, stdout=subprocess.PIPE, universal_newlines=True, shell=True
+    )
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield str(stdout_line).strip()
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, command)
+
+
+def main():
+    """
+    Main function to execute the smartspim quantification
+    in code ocean
+    """
+    image_path = quantification.main()
+    bucket_path = "aind-open-data"
+
+    output_folder = "/results"
+
+    dataset_folder = str(sys.argv[2]).split("/")[2]
+    channel_name = str(sys.argv[4])
+
+    dataset_name = dataset_folder + f"/processed/Quantification/{channel_name}"
+    s3_path = f"s3://{bucket_path}/{dataset_name}"
+
+    for out in execute_command_helper(
+        f"aws s3 mv --recursive {output_folder} {s3_path}"
+    ):
+        print(out)
+
+    save_string_to_txt(
+        f"Results of cell quantification saved in: {s3_path}",
+        "/root/capsule/results/output_quantification.txt",
+    )
+
+
+if __name__ == "__main__":
+    main()
