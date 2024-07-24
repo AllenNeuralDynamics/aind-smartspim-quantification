7d4b35f34de48c839e4c52442d699533e37640d9
Nicholas Lusk <nicholas.lusk@alleninstitute.org>
2024-07-24 18:18:14 +0000

    Edited quantification.py

--- code/aind_smartspim_quantification/quantification.py
+++ code/aind_smartspim_quantification/quantification.py
@@ -174,8 +174,8 @@ def read_aws_xml(seg_path: PathLike, reg_dims: list, ds: int, orient: str, insti
             cells.append(
                 (
                     reg_dims[0] - (int(cell['MarkerZ']) / ds),
-                    reg_dims[1] - (int(cell['MarkerY'])  / ds),
-                    int(cell['MarkerX']) / ds,
+                    reg_dims[1] - (int(cell['MarkerY']) / ds),
+                    reg_dims[2] - (int(cell['MarkerX']) / ds),
                 )
             )
 
