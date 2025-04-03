19de6700e09da6b92e722193c46f2431cbad59e6
nal10 <nicholas.lusk@alleninstitute.org>
2025-04-03 16:58:35 +0000

    Edited generate_ccf_cell_count.py

--- code/aind_smartspim_quantification/utils/generate_ccf_cell_count.py
+++ code/aind_smartspim_quantification/utils/generate_ccf_cell_count.py
@@ -220,7 +220,7 @@ def generate_precomputed_cells(cells, precompute_path, configs):
 
     metadata = {
         "@type": "neuroglancer_annotations_v1",
-        "dimensions": configs['dimensions'],
+        "dimensions": dict((key, configs['dimensions'][key]) for key in ('z', 'y', 'x')),
         "lower_bound": [float(x) for x in l_bounds],
         "upper_bound": [float(x) for x in u_bounds],
         "annotation_type": "point",
